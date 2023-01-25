c65e5593d0dfead8f9c2016eac2c1a918fce909c
Mateus Maranhão <mateusmaranhao0210@gmail.com>
2023-01-24 21:07:23 -0300

    adicionando s-accordion

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var icon = document.querySelectorAll(".icon");
+var question = document.querySelectorAll(".question-group");
+
+for(i = 0; i < question.length; i++) {
+    question[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+    });
+
+    question.forEach((drop) => {
+        drop.addEventListener("click", () => {
+            drop.classList.toggle("clicked");
+        });
+    });
+}
